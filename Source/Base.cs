305ceeede4c7ab6352f33ade65c813e04fcf1f82
Lana Avery <memekoten@gmail.com>
2025-12-23 14:43:35 +1100

    Changed the world comp caching

--- Source/Base.cs
+++ Source/Base.cs
@@ -1,10 +1,12 @@
 ﻿using DualWield.Settings;
 using DualWield.Storage;
+using HarmonyLib;
 using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RimWorld.Planet;
 using UnityEngine;
 using Verse;
 
@@ -35,8 +37,21 @@ namespace DualWield
 
         public ExtendedDataStorage GetExtendedDataStorage()
         {
-            return _extendedDataStorage ?? (_extendedDataStorage = Find.World.GetComponent<ExtendedDataStorage>());
+            return _extendedDataStorage ?? Find.World.GetComponent<ExtendedDataStorage>();
         }
-        
+
+        public void RefreshExtendedDataStorage()
+        {
+            _extendedDataStorage = Find.World.GetComponent<ExtendedDataStorage>();
+        }
+    }
+
+    [HarmonyPatch(typeof(World))]
+    [HarmonyPatch(nameof(World.FinalizeInit))]
+    [HarmonyPatch(new System.Type[] { typeof(bool) })]
+    internal static class ExtendedDataStorageLoader
+    {
+        [HarmonyPostfix]
+        private static void LoadComp(World __instance, bool loadFrom) => Base.Instance.RefreshExtendedDataStorage();
     }
 }
